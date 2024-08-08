9cf8942657651b5abc3ca9576b2afaf26a2e1fab
Rene <rene.golz84@gmail.com>
2024-08-08 13:48:49 +0200

    Update setup_and_run.sh

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -5,33 +5,41 @@ PYTHON_SCRIPT_URL="https://raw.githubusercontent.com/dismaster/NeoxaMultiNodeIns
 SCRIPT_NAME="install_neoxa_smartnodes.py"
 
 # Update package list and install prerequisites
-echo "Updating package list and installing prerequisites..."
-sudo apt-get update -y
-sudo apt-get install -y wget curl git python3-psutil python3-colorama screen
+echo -e "\e[96mUpdating package list and installing prerequisites...\e[0m"
+sudo apt-get update -y > /dev/null
+sudo apt-get install -y wget curl git python3-psutil python3-colorama screen > /dev/null
 
 # Install Python if not installed
 if ! command -v python3 &> /dev/null; then
-    echo "Python is not installed. Installing Python..."
-    sudo apt-get install -y python3 python3-venv python3-pip
+    echo -e "\e[93mPython is not installed. Installing Python...\e[0m"
+    sudo apt-get install -y python3 python3-venv python3-pip > /dev/null
 else
-    echo "Python is already installed."
+    echo -e "\e[92mPython is already installed.\e[0m"
 fi
 
 # Ensure pip is installed and updated
 if ! command -v pip3 &> /dev/null; then
-    echo "pip is not installed. Installing pip..."
-    sudo apt-get install -y python3-pip
+    echo -e "\e[93mpip is not installed. Installing pip...\e[0m"
+    sudo apt-get install -y python3-pip > /dev/null
 else
-    echo "pip is already installed. Upgrading pip..."
-    sudo pip3 install --upgrade pip
+    echo -e "\e[92mpip is already installed. Upgrading pip...\e[0m"
+    sudo pip3 install --upgrade pip > /dev/null
+fi
+
+# Install screen if not installed
+if ! command -v screen &> /dev/null; then
+    echo -e "\e[93mscreen is not installed. Installing screen...\e[0m"
+    sudo apt-get install -y screen > /dev/null
+else
+    echo -e "\e[92mscreen is already installed.\e[0m"
 fi
 
 # Download the Python script from GitHub
-echo "Downloading the Python script from GitHub..."
-wget -O $SCRIPT_NAME $PYTHON_SCRIPT_URL
+echo -e "\e[96mDownloading the Python script from GitHub...\e[0m"
+wget -O $SCRIPT_NAME $PYTHON_SCRIPT_URL > /dev/null
 
 # Run the Python script
-echo "Running the Python script..."
+echo -e "\e[96mRunning the Python script...\e[0m"
 python3 $SCRIPT_NAME
 
-echo "Setup and execution completed."
+echo -e "\e[92mSetup and execution completed.\e[0m"
