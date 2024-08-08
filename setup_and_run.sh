2dfba2018e551a056079e1e5146b8fdbd35bb570
Rene <rene.golz84@gmail.com>
2024-08-08 13:41:04 +0200

    Update setup_and_run.sh

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -3,12 +3,11 @@
 # Variables
 PYTHON_SCRIPT_URL="https://raw.githubusercontent.com/dismaster/NeoxaMultiNodeInstaller/main/install_neoxa_smartnodes.py"
 SCRIPT_NAME="install_neoxa_smartnodes.py"
-VENV_DIR="$HOME/neoxa_venv"
 
 # Update package list and install prerequisites
 echo "Updating package list and installing prerequisites..."
 sudo apt-get update -y
-sudo apt-get install -y wget curl git python3-venv
+sudo apt-get install -y wget curl git python3-psutil python3-colorama screen
 
 # Install Python if not installed
 if ! command -v python3 &> /dev/null; then
@@ -27,25 +26,6 @@ else
     sudo pip3 install --upgrade pip
 fi
 
-# Install screen if not installed
-if ! command -v screen &> /dev/null; then
-    echo "screen is not installed. Installing screen..."
-    sudo apt-get install -y screen
-else
-    echo "screen is already installed."
-fi
-
-# Create a virtual environment
-echo "Creating a virtual environment..."
-python3 -m venv $VENV_DIR
-
-# Activate the virtual environment
-source $VENV_DIR/bin/activate
-
-# Install required Python packages
-echo "Installing required Python packages..."
-pip install colorama psutil
-
 # Download the Python script from GitHub
 echo "Downloading the Python script from GitHub..."
 wget -O $SCRIPT_NAME $PYTHON_SCRIPT_URL
@@ -53,8 +33,5 @@ wget -O $SCRIPT_NAME $PYTHON_SCRIPT_URL
 # Run the Python script
 echo "Running the Python script..."
 python3 $SCRIPT_NAME
-
-# Deactivate the virtual environment
-deactivate
 
 echo "Setup and execution completed."
